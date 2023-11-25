f84da94bf2150f5816a0186463fe87884d276ddb
Vladislav Kharin <vladkharin19@gmail.com>
2023-11-25 10:38:07 +0300

    second service

--- orgmanagement/src/main/java/com/vladhacksmile/orgmanagement/model/ResponseMapper.java
+++ orgmanagement/src/main/java/com/vladhacksmile/orgmanagement/model/ResponseMapper.java
@@ -1,10 +1,11 @@
 package com.vladhacksmile.orgmanagement.model;
 
 import com.vladhacksmile.orgmanagement.model.result.Result;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseMapper {
     public static <T> ResponseEntity<Result<T>> map(Result<T> result) {
-        return new ResponseEntity<>(result, result.getStatus().getHttpStatus());
+        return new ResponseEntity<>(result, HttpStatus.valueOf(result.getStatus().getHttpStatus()));
     }
 }
