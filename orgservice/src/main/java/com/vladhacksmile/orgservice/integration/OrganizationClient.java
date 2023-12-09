5da41a3f475c30f53eb8207b81a154f722bfca47
Xaviontt <xavion@list.ru>
2023-12-09 15:12:15 +0300

    ssl

--- orgservice/src/main/java/com/vladhacksmile/orgservice/integration/OrganizationClient.java
+++ orgservice/src/main/java/com/vladhacksmile/orgservice/integration/OrganizationClient.java
@@ -1,5 +1,6 @@
 package com.vladhacksmile.orgservice.integration;
 
+import com.vladhacksmile.orgservice.config.AppConfig;
 import com.vladhacksmile.orgservice.model.entity.Employee;
 import com.vladhacksmile.orgservice.model.entity.OrganizationDTO;
 import com.vladhacksmile.orgservice.model.result.Result;
@@ -12,15 +13,52 @@ import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+
 @Stateless
 public class OrganizationClient {
     private Client client;
     private final String serviceUrl = "https://localhost:8080";
 
+    public static SSLContext createSSLContext(String truststorePath, String truststorePassword) throws Exception {
+        KeyStore truststore = KeyStore.getInstance("PKCS12");
+
+        try (InputStream truststoreInput = new FileInputStream(truststorePath)) {
+            truststore.load(truststoreInput, truststorePassword.toCharArray());
+        }
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(truststore);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, tmf.getTrustManagers(), null);
+
+        return sslContext;
+    }
+
+    public static Client createSSLClient() throws Exception {
+        SSLContext sslContext = createSSLContext(
+                AppConfig.getProperty("truststore.path"),
+                AppConfig.getProperty("truststore.key")
+        );
+
+        return ClientBuilder.newBuilder()
+                .sslContext(sslContext)
+                .build();
+    }
+
     public Result<OrganizationDTO> getOrganizationById(long id) {
         String url = serviceUrl + "/organizations/" + id;
         try {
-            client = ClientBuilder.newClient();
+            try {
+                client = createSSLClient();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
 
             Response response = client.target(url).request(MediaType.APPLICATION_JSON_TYPE).get();
 
@@ -37,7 +75,11 @@ public class OrganizationClient {
     public Result<Employee> addEmployee(Employee employee) {
         String url = serviceUrl + "/employees";
         try {
-            client = ClientBuilder.newClient();
+            try {
+                client = createSSLClient();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
 
             Response response = client.target(url).request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(employee));
 
@@ -54,7 +96,11 @@ public class OrganizationClient {
     public Result<Integer> migrateEmployees(Long organizationId1, Long organizationId2) {
         String url = serviceUrl + "/employees/migrate/" + organizationId1 + "/" + organizationId2;
         try {
-            client = ClientBuilder.newClient();
+            try {
+                client = createSSLClient();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
 
             Response response = client.target(url).request(MediaType.APPLICATION_JSON_TYPE).post(null);
 
@@ -71,7 +117,11 @@ public class OrganizationClient {
     public Result<OrganizationDTO> deleteOrganizationById(long id) {
         String url = serviceUrl + "/organizations/" + id;
         try {
-            client = ClientBuilder.newClient();
+            try {
+                client = createSSLClient();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
 
             Response response = client.target(url).request(MediaType.APPLICATION_JSON_TYPE).delete();
 
