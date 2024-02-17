621a6619b8a40439337a7e717915dbc4d76b0e83
Vladislav Kharin <vladkharin19@gmail.com>
2024-02-17 11:48:37 +0300

    stable mb

--- orgmanagement_service/src/main/java/com/vladhacksmile/orgmanagement/OrgManagementApplication.java
+++ orgmanagement_service/src/main/java/com/vladhacksmile/orgmanagement/OrgManagementApplication.java
@@ -2,19 +2,13 @@ package com.vladhacksmile.orgmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EntityScan
-@EnableTransactionManagement
 @EnableDiscoveryClient
 @RestController
 public class OrgManagementApplication {
